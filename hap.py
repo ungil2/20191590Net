c0604181f63b4989dde2048cb4424966237ca8a8
ung il sun <tjdndlf1100@naver.com>
2024-03-19 11:45:16 +0900

    message1

---
+++ hap.py
@@ -0,0 +1,10 @@
+score = int(input("정수 입력"))
+if score >= 90 :
+    print("성적 : A")
+    print("장학금 수여")
+
+age_0 = 22
+age_1 = 18
+age = int(input('your age? '))
+if age < age_0 and age > age_1:
+    print("당신의 나이는 18~22세 사이입니다")
