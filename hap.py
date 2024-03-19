2cfd5e6abbead5cb5f50d2f648b05d74dc8aa221
ung il sun <tjdndlf1100@naver.com>
2024-03-19 16:31:28 +0900

    1

--- hap.py
+++ hap.py
@@ -1,10 +1,9 @@
-score = int(input("정수 입력"))
-if score >= 90 :
-    print("성적 : A")
-    print("장학금 수여")
+n = int(input('Number: '))
+res = ''
 
-age_0 = 22
-age_1 = 18
-age = int(input('your age? '))
-if age < age_0 and age > age_1:
-    print("당신의 나이는 18~22세 사이입니다")
+while n != 0:
+    m = n % 2
+    res = str(m) + res
+    n = n // 2
+
+print(res)
